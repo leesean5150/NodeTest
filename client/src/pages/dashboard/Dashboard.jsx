60a1a07785fa322f3e8e0bb865a1e3f86890fd9c
Lee Sean <leesean18082001@gmail.com>
2024-06-23 00:34:07 +0800

    completed conditional rendering example on dashbaord

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,19 +1,21 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import env_config from "../../api/env_config";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
+  const [role, setRole] = useState("");
 
   useEffect(() => {
     axios.get(env_config.env + "auth/verify").then((response) => {
       if (!response.data.status) {
         navigate("/login");
       }
+      setRole(response.data.role);
     });
-  }, [navigate]);
+  }, []);
 
   const handleLogout = () => {
     axios
@@ -27,11 +29,64 @@ const Dashboard = () => {
         console.log(err);
       });
   };
+
+  console.log(role);
+
   return (
-    <div>
+    <>
       Dashboard
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: 120,
+            height: 120,
+            margin: 20,
+            background: role === "admin" ? "#8055DC" : "#D9D9D9",
+            borderRadius: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Admin
+        </div>
+        <div
+          style={{
+            width: 120,
+            height: 120,
+            margin: 20,
+            background: role === "trainer" ? "#8055DC" : "#D9D9D9",
+            borderRadius: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Trainer
+        </div>
+        <div
+          style={{
+            width: 120,
+            height: 120,
+            margin: 20,
+            background: role === "client" ? "#8055DC" : "#D9D9D9",
+            borderRadius: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Client
+        </div>
+      </div>
       <button onClick={handleLogout}>Logout</button>
-    </div>
+    </>
   );
 };
 
