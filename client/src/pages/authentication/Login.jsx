cbb675a217ce4f99d92f0f4a67603a0f4e4b3e7a
leesean5150 <sean.lee@nexusfrontier.tech>
2024-06-25 15:12:30 +0800

    completed login validation for individual roles

--- client/src/pages/authentication/Login.jsx
+++ client/src/pages/authentication/Login.jsx
@@ -8,6 +8,7 @@ import env_config from "../../api/env_config";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginType, setLoginType] = useState("client");
 
   const navigate = useNavigate();
 
@@ -16,7 +17,7 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post(env_config.env + "auth/login", {
+      .post(env_config.env + "auth/login/" + loginType, {
         username,
         password,
       })
@@ -31,9 +32,61 @@ const Login = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <div className="sign-up-container" style={{}}>
       <form className="sign-up-form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
+        <h2 style={{}}>Login</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <button
+            style={{
+              background: loginType === "client" ? "#3498db" : "#D9D9D9",
+              borderRadius: 20,
+              display: "display: inline-block",
+              justifyContent: "center",
+              margin: 10,
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              setLoginType("client");
+            }}
+          >
+            Client
+          </button>
+          <button
+            style={{
+              background: loginType === "admin" ? "#3498db" : "#D9D9D9",
+              borderRadius: 20,
+              display: "display: inline-block",
+              justifyContent: "center",
+              margin: 10,
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              setLoginType("admin");
+            }}
+          >
+            Admin
+          </button>
+          <button
+            style={{
+              background: loginType === "trainer" ? "#3498db" : "#D9D9D9",
+              borderRadius: 20,
+              display: "display: inline-block",
+              justifyContent: "center",
+              margin: 10,
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              setLoginType("trainer");
+            }}
+          >
+            Trainer
+          </button>
+        </div>
         <label htmlFor="username">Username:</label>
         <input
           type="text"
